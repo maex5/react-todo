e7764a9925079605385d477c0fee7c5bad41e8eb
Max Salmi <max.salmi@me.com>
2017-01-30 20:48:25 +0200

    Refactor TodoList, Todo and Add todo to work with redux

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -1,11 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Provider} = require('react-redux');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
-var TodoList = require('TodoList');
-var Todo = require('Todo');
+import {configure} from 'configureStore';
+import ConnectedTodoList, {TodoList} from 'TodoList';
+import ConnectedTodo, {Todo} from 'Todo';
 
 describe('TodoList', () => {
   it('should exist', () => {
@@ -15,14 +17,28 @@ describe('TodoList', () => {
   it('should render on Todo component for each todo item', () => {
     var todos = [{
       id: 1,
-      text: 'something'
+      text: 'something',
+      completed: false,
+      completedAt: undefined,
+      createdAt: 123
     },
     {
       id: 2,
-      text: 'something else'
+      text: 'something else',
+      completed: false,
+      completedAt: undefined,
+      createdAt: 123
     }];
-    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
-    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+    var store = configure({
+      todos
+    });
+    var provider = TestUtils.renderIntoDocument(
+      <Provider store={store}>
+        <ConnectedTodoList/>
+      </Provider>
+    );
+    var todoList = TestUtils.scryRenderedComponentsWithType(provider, ConnectedTodoList)[0];
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, ConnectedTodo);
 
     expect(todosComponents.length).toBe(todos.length);
   });
